ac99245f764a145ac0dc3e73c451b7dae4e67bb5
Saurabh9172 <9172169879misal@gmail.com>
2025-08-18 09:44:43 +0530

    feedbackschedule form

--- Feedback_Manangement_System/src/App.jsx
+++ Feedback_Manangement_System/src/App.jsx
@@ -2,14 +2,22 @@ import { useState } from 'react'
 import FeedbackTypeForm from "./components/FeedbackTypeForm";
 import AddQuestionForm from './components/AddQuestionForm';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScheduleFeedbackForm from './components/ScheduleFeedbackForm';
+import EditGroup from './components/EditGroup';
 function App() {
   const [questions, setQuestions] = useState([]);
+  const [groups,setGroups]=useState([
+    {id:1,groupName:"D1",staffName:"John Doe"},
+    {id:2,groupName:"D2",staffName:"Jane Smith"}
+  ]);
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<FeedbackTypeForm questions={questions} setQuestions={setQuestions}/>}></Route>
-        <Route path="/add-question" element={<AddQuestionForm questions={questions} setQuestions={setQuestions}/>}></Route>
+        <Route path='/' element={<ScheduleFeedbackForm groups={groups} setGroups={setGroups}/>}></Route>
+        {/* <Route path="/" element={<FeedbackTypeForm questions={questions} setQuestions={setQuestions}/>}></Route> */}
+        {/* <Route path="/add-question" element={<AddQuestionForm questions={questions} setQuestions={setQuestions}/>}></Route> */}
+        <Route path="/edit-group/:id" element={<EditGroup groups={groups} setGroups={setGroups}/>}></Route>     
       </Routes>
     </Router>
   )
