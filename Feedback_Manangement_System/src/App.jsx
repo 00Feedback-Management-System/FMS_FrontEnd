5fb87435307d5b7e2ae7ac09e2ec1440b9044150
shriramjoshi7737 <136720407+shriramjoshi7737@users.noreply.github.com>
2025-08-19 13:32:58 +0530

    route add and folder outside of feedback

--- Feedback_Manangement_System/src/App.jsx
+++ Feedback_Manangement_System/src/App.jsx
@@ -12,6 +12,9 @@ import EditGroup from './pages/feedback/EditGroup';
 import ScheduleFeedbackForm from './pages/feedback/ScheduleFeedbackForm';
 import FacultyFeedbackSummary from './pages/feedback/FacultyFeedbackSummary';
 import DashboardLayout from './components/DashboardLayout';
+import FeedbackDashboard from './pages/Dashboard/Feedbackdashbaord';
+import Login from './pages/Login/Login';
+import Faculty_Feedback_summary from './pages/FeedbackReport/Faculty_Feedback_summary';
 
 function App() {
   const [questions, setQuestions] = useState([]);
@@ -22,7 +25,8 @@ function App() {
   return (
    <Router>
       <Routes>
-        <Route path="/" element={<FeedbackTypeList />} />
+        <Route path="/" element={ <Login/>} />
+        <Route path="/feedback-type-list" element={<FeedbackTypeList />} />
         <Route path="/Schedule-Feedback-List" element={<ScheduleFeedbackList />} />
         <Route path="/student-list/:scheduleId" element={<StudentList />}/>
         <Route path="/remaining/:scheduleId" element={<RemainingStudent />}/>
@@ -30,9 +34,12 @@ function App() {
         <Route path="/add-question" element={<AddQuestionForm questions={questions} setQuestions={setQuestions}/>}/>
         <Route path="/edit-group/:id" element={<EditGroup groups={groups} setGroups={setGroups} />}/>
         <Route path='/schedule-feedback-form' element={<ScheduleFeedbackForm groups={groups} setGroups={setGroups}/>}></Route>
-        <Route path="/student-feedback-form" element={<StudentFeedbackForm/>} />
+        <Route path="/student-feedback-form" element={<StudentFeedbackForm/>}/>
         <Route path="/faculty-feedback-summary" element={<FacultyFeedbackSummary />} />
         <Route path="/dashboard" element={<DashboardLayout />} />
+        <Route path="/feedback-dashboard" element={<FeedbackDashboard />} />
+        <Route path="/faculty_summary" element={<Faculty_Feedback_summary />} />
+
       </Routes>
    </Router>
     );
